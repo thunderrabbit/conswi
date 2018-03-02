4a4bef9fbf938a739e0d1054071db2e1540c4370
Rob Nugen <rob@robnugen.com>
2018-03-02 20:13:32 +0900

    Remove excess debug lines

--- tiles/PlayerArea2D.gd
+++ tiles/PlayerArea2D.gd
@@ -8,7 +8,6 @@ signal exited
 var is_a_game_piece = true		# helps detect sprites for group actions
 var is_swipable = false			# will prevent clicks from signalling GameScene
 var tile_type = null
-
 func _ready():
 	self.connect("clicked", get_node("/root/GameNode2D"), "piece_clicked", [])
 	self.connect("unclicked", get_node("/root/GameNode2D"), "piece_unclicked", [])
@@ -20,7 +19,6 @@ func set_tile_type(type):
 
 # called after piece gets nailed to board
 func become_swipable():
-	print("become swipable")
 	# I would think set_process_input(false) should do the same thing
 	# but it did not seem to work.
 	is_swipable = true
@@ -29,25 +27,18 @@ func _on_PlayerArea2D_input_event( viewport, event, shape_idx ):
 	if not is_swipable:
 		return
 	if event is InputEventScreenTouch or event is InputEventMouseButton:
-		print(event.pressed)
-		print(Helpers.pixels_to_slot(get_position()))
-
 		if event.pressed:
 			emit_signal("clicked", Helpers.pixels_to_slot(get_position()), tile_type)
 		else: # not event.pressed:
 			emit_signal("unclicked")
 
 func _on_PlayerArea2D_mouse_entered():
-	print("on _on_PlayerArea2D_mouse_entered event")
 	if not is_swipable:
 		return
-	print(Helpers.pixels_to_slot(get_position()))
 	emit_signal("entered", Helpers.pixels_to_slot(get_position()), tile_type)
 
 func _on_PlayerArea2D_mouse_exited():
-	print("on _on_PlayerArea2D_mouse_exited event")
 	if not is_swipable:
 		return
-	print(Helpers.pixels_to_slot(get_position()))
 	emit_signal("exited", Helpers.pixels_to_slot(get_position()), tile_type)
 
