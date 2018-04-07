3786324409da683a5d1aeb2948b8e233ddecf1f8
Rob Nugen <rob@robnugen.com>
2018-04-07 14:50:48 +0900

    Allow Tile names to be accessed with "ITEM_NAME"
    because `const ITEM_NAME` is local to TileDatabase
    and therefore useless anywhere else

--- helpers/TileDatabase.gd
+++ helpers/TileDatabase.gd
@@ -1,41 +1,40 @@
 extends Node
 
-const ITEM_NAME         = 0
 const ITEM_SPRITE       = 3
 
 var tiles = [
 	{
-		ITEM_NAME : "Pig",
+		"ITEM_NAME" : "Pig",
 		ITEM_SPRITE : G.TYPE_PIG,
 		"ITEM_COLOR" : Color(1, 1.0, 0.5, 1.0),
 	},
 	{
-		ITEM_NAME : "Sheep",
+		"ITEM_NAME" : "Sheep",
 		ITEM_SPRITE : G.TYPE_SHEEP,
 		"ITEM_COLOR" : Color(0, 1.0, 0.5, 1.0),
 	},
 	{
-		ITEM_NAME : "Panda",
+		"ITEM_NAME" : "Panda",
 		ITEM_SPRITE : G.TYPE_PANDA,
 		"ITEM_COLOR" : Color(1, 1, 1, 1),
 	},
 	{
-		ITEM_NAME : "Dog",
+		"ITEM_NAME" : "Dog",
 		ITEM_SPRITE : G.TYPE_DOG,
 		"ITEM_COLOR" : Color(0, 0, 1, 1),
 	},
 	{
-		ITEM_NAME : "Cow",
+		"ITEM_NAME" : "Cow",
 		ITEM_SPRITE : G.TYPE_COW,
 		"ITEM_COLOR" : Color(1.0, 0, 0.5, 1.0),
 	},
 	{
-		ITEM_NAME : "Cat",
+		"ITEM_NAME" : "Cat",
 		ITEM_SPRITE : G.TYPE_CAT,
 		"ITEM_COLOR" : Color(1.0, 0, 0.5, 1.0),
 	},
 	{
-		ITEM_NAME : "Bear",
+		"ITEM_NAME" : "Bear",
 		ITEM_SPRITE : G.TYPE_BEAR,
 		"ITEM_COLOR" : Color(1.0, 0, 0.5, 1.0),
 	},
