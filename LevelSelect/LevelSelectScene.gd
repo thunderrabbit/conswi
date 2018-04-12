489f3b606a6813bbd2ddd217a67b0ba0b000927f
Rob Nugen <rob@robnugen.com>
2018-04-12 11:55:12 +0900

    Tabify level select scene
    because tab is default on the editor.
    I wonder if I can switch it to spaces.

--- LevelSelect/LevelSelectScene.gd
+++ LevelSelect/LevelSelectScene.gd
@@ -17,27 +17,29 @@ var num_buttons       = 12
 
 const LevelSelectButton = preload("LevelSelectButton.tscn")
 
-func _ready():
-    var button_loc = Vector2(0,0)
-    for level in range(1,num_buttons+1):
-        button_loc = level_to_pixels(level)
-        var level_but = LevelSelectButton.instance()
-        level_but.set_position(button_loc)
-        level_but.set_level(level)
-        level_but.set_size(Vector2(SLOT_SIZE,SLOT_SIZE))
-        level_but.set_button_type(G.TYPE_DOG)
-        level_but.set_qty_active(2)
-        level_but.scale_to(Vector2(2,2))
-        level_but.connect("pressed",self,"_on_Button_pressed",[level])
-        add_child(level_but)
+func _init():
+	var button_loc = Vector2(0,0)
+	print("buttons for level")
+	for level in range(1,num_buttons+1):
+		print("button for level")
+		button_loc = level_to_pixels(level)
+		var level_but = LevelSelectButton.instance()
+		level_but.set_position(button_loc)
+		level_but.set_level(level)
+		level_but.set_size(Vector2(SLOT_SIZE,SLOT_SIZE))
+		level_but.set_button_type(G.TYPE_DOG)
+		level_but.set_qty_active(2)
+		level_but.scale_to(Vector2(2,2))
+		level_but.connect("pressed",self,"_on_Button_pressed",[level])
+		add_child(level_but)
 
 func level_to_pixels(level):
 	# level-1 because we start with 1 above.  Otherwise gap appears in top left corner
-    level = level - 1
-    var slot = Vector2(level % buttons_across, level / buttons_across)
-    return Vector2(left_space+(SLOT_SIZE + slot_gap_h)*(slot.x), 
-                    top_space+(SLOT_SIZE + slot_gap_v)*(slot.y))
+	level = level - 1
+	var slot = Vector2(level % buttons_across, level / buttons_across)
+	return Vector2(left_space+(SLOT_SIZE + slot_gap_h)*(slot.x),
+	                top_space+(SLOT_SIZE + slot_gap_v)*(slot.y))
 
 func _on_Button_pressed(level):
-    Helpers.requested_level = level
-    get_node("/root/SceneSwitcher").goto_scene("res://Game.tscn")
+	Helpers.requested_level = level
+	get_node("/root/SceneSwitcher").goto_scene("res://Game.tscn")
