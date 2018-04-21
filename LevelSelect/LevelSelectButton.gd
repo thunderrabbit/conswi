d829be5515d6524dba2e03e5b2ff233a750c5f75
Rob Nugen <rob@robnugen.com>
2018-04-21 20:40:26 +0900

    Remove unneeded icons from base select button

--- LevelSelect/LevelSelectButton.gd
+++ LevelSelect/LevelSelectButton.gd
@@ -5,8 +5,9 @@ var my_type_string = ""
 var icons = ["left", "center", "right"]
 
 func set_level(level):
-	var path_to_num = String("res://images/levelselect/numbers/" + String(level).pad_zeros(2) + ".png")
-	$number.set_texture(load(path_to_num))
+	pass
+#	var path_to_num = String("res://images/levelselect/numbers/" + String(level).pad_zeros(2) + ".png")
+#	$number.set_texture(load(path_to_num))
 
 func set_button_type(type = G.TYPE_DOG):
 	self.my_type = type
@@ -25,8 +26,8 @@ func set_qty_active(qty = 3):
 
 func scale_to(scale):
 	$background.set_scale(scale)
-	$number.set_scale(scale)
-	$plate.set_scale(scale)
+#	$number.set_scale(scale)
+#	$plate.set_scale(scale)
 	$icon1.set_scale(scale)
 	$icon2.set_scale(scale)
 	$icon3.set_scale(scale)
