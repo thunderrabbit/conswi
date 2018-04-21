53eceb6c4cb8728e3d123d5d37e9924adddbd0f8
Rob Nugen <rob@robnugen.com>
2018-04-21 21:11:45 +0900

    Import bunch of images for new way to handle select buttons

--- LevelSelect/LevelSelectButton.gd
+++ LevelSelect/LevelSelectButton.gd
@@ -2,20 +2,18 @@ extends TextureButton
 
 var my_type = G.TYPE_BEAR
 var my_type_string = ""
-var icons = ["left", "center", "right"]
 
 func set_level(level):
-	pass
-#	var path_to_num = String("res://images/levelselect/numbers/" + String(level).pad_zeros(2) + ".png")
-#	$number.set_texture(load(path_to_num))
+	var path_to_num = String("res://images/levelselect/numberedplates/level_" + String(level).pad_zeros(2) + "_bg_x1.png")
+	$background.set_texture(load(path_to_num))
 
 func set_button_type(type = G.TYPE_DOG):
 	self.my_type = type
 	self.my_type_string = TileDatabase.tiles[my_type]["ITEM_NAME"]
 
 func _set_icon_active(icon_num):
-	var which_icon = icons[icon_num - 1]	# -1 to account for zero-indexed array `icons`
-	var path_to_icon = String("res://images/levelselect/icons/" + self.my_type_string + "_" + which_icon + ".png")
+	var which_icon = icon_num	# -1 to account for zero-indexed array `icons`
+	var path_to_icon = String("res://images/levelselect/icons/level_01_" + self.my_type_string + String(which_icon) + "_" + "1x.png")
 	get_node("icon"+String(icon_num)).set_texture(load(path_to_icon))
 
 func set_qty_active(qty = 3):
@@ -26,8 +24,9 @@ func set_qty_active(qty = 3):
 
 func scale_to(scale):
 	$background.set_scale(scale)
-#	$number.set_scale(scale)
-#	$plate.set_scale(scale)
+	$icon1.set_position($background.position)	# make sure icons line up with background
+	$icon2.set_position($background.position)	# make sure icons line up with background
+	$icon3.set_position($background.position)	# make sure icons line up with background
 	$icon1.set_scale(scale)
 	$icon2.set_scale(scale)
 	$icon3.set_scale(scale)
