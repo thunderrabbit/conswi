633274bfaa09f889d517803eafa71dac6beb047b
Rob Nugen <rob@robnugen.com>
2018-04-29 23:16:49 +0900

    Proof concept save data to disk with Savior.gd

--- io/Savior.gd
+++ io/Savior.gd
@@ -1,26 +1,97 @@
 extends Node
 
 var savegame = File.new() # file
-var save_path = "user://savegame.save" # place of the file
-var save_data = {"highscore": 7770} #variable to store data
+var encryptgame = File.new() # file
+const SAVE_TEXT = "user://save_game_data.dat"
+const SAVE_ENCRYPTED = "user://save_game_data.bin"
+var password
 
+var save_dict = {} # variable to store data
+
+####################################################
+#
+#	On first run, create local files to save the data
+#
+#   On subsequent runs, load previously saved data
 func _ready():
-	if not savegame.file_exists(save_path):
-		create_save()
+	self.password = OS.get_unique_id()
+	self._initialize_data()				# Don't need to do this if local files exist
+	self._create_files_prn()			# will save files if they do not exist
+	self.read_savegame()				# will read default at first, then updated value if anything has been saved
+	self.save_high_score(55)
 
-func create_save():
-	savegame.open(save_path, File.WRITE)
-	savegame.store_var(save_data)
-	savegame.close()
+####################################################
+#
+#   Create local files if necessary
+func _create_files_prn():
+	if not savegame.file_exists(SAVE_TEXT):
+		self._create_save(SAVE_TEXT,false)		# false = no encryption
+	if not encryptgame.file_exists(SAVE_ENCRYPTED):
+		self._create_save(SAVE_ENCRYPTED)
 
-func save(high_score):
-	save_data["highscore"] = high_score #data to save
-	savegame.open(save_path, File.WRITE) #open file to write
-	savegame.store_var(save_data) #store the data
-	savegame.close() # close the file
+####################################################
+#
+#   Save data in one of two formats:
+#      secure binary format (default)
+#      debug-friendly text format
+func _create_save(filename, encrypt = true):
+	var create_file = File.new()
+	if(encrypt):
+		create_file.open_encrypted_with_pass(filename, File.WRITE, self.password)
+	else:
+		create_file.open(filename, File.WRITE)
+	create_file.store_string(to_json(self.save_dict))
+	create_file.close()
 
+####################################################
+#
+#   Save both visible and encrypted files
+func _save():
+	self._create_save(SAVE_TEXT,false)		# false = no encryption
+	self._create_save(SAVE_ENCRYPTED)
+
+####################################################
+#
+#   "low" level function just saves value and writes to disk
+func _write_value(field, value):
+	self.save_dict[field] = value;		# Add value to dictionary
+	self._save()						# Save data to disk
+
+####################################################
+#
+#   "low" level function just returns value
+func _read_value(field):
+
+	if(self.save_dict.has(field)):
+		return self.save_dict[field]
+	else:
+		return 0
+
+####################################################
+#
+#   "mid" level function writes value if it is larger
+#    than previously saved value 
+func _write_if_larger(field, value):
+	if(value > int(self._read_value(field))):
+		_write_value(field, value)
+
+func save_high_score(score):
+	self._write_if_larger("highscore",score)
+
+####################################################
+#
+#   Only load from encrypted file
+#   If anyone tampers with local text file,
+#      they cannot change their score or w/e
 func read_savegame():
-	savegame.open(save_path, File.READ) #open the file
-	save_data = savegame.get_var() #get the value
-	savegame.close() #close the file
-	return save_data["highscore"] #return the value
+	var read_file = File.new()
+	read_file.open_encrypted_with_pass(self.SAVE_ENCRYPTED, File.READ, self.password)
+	var save_data = read_file.get_line()	# All data is in a single line
+	read_file.close()
+	self.save_dict = parse_json(save_data)
+	print(save_data)
+	print(self.save_dict)
+
+func _initialize_data():
+	self.save_dict["current_world"] = 0;
+	self.save_dict["allowed_world"] = 0;
