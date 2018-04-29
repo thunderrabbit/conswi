306741b79758973381d32365c2bdf4e3b8014fcd
Rob Nugen <rob@robnugen.com>
2018-04-29 18:33:05 +0900

    Ruff draft of save code
    https://godotengine.org/qa/2080/save-record?show=2082#a2082

--- io/Savior.gd
+++ io/Savior.gd
@@ -1,15 +1,26 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var savegame = File.new() # file
+var save_path = "user://savegame.save" # place of the file
+var save_data = {"highscore": 7770} #variable to store data
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	if not savegame.file_exists(save_path):
+		create_save()
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func create_save():
+	savegame.open(save_path, File.WRITE)
+	savegame.store_var(save_data)
+	savegame.close()
+
+func save(high_score):
+	save_data["highscore"] = high_score #data to save
+	savegame.open(save_path, File.WRITE) #open file to write
+	savegame.store_var(save_data) #store the data
+	savegame.close() # close the file
+
+func read_savegame():
+	savegame.open(save_path, File.READ) #open the file
+	save_data = savegame.get_var() #get the value
+	savegame.close() #close the file
+	return save_data["highscore"] #return the value
