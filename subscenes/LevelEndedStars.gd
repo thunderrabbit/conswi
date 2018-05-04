84876d47e2b76e9ae485872af6db3e3083901a1e
Rob Nugen <rob@robnugen.com>
2018-05-04 23:20:37 +0900

    Determine what to do after level ends

--- subscenes/LevelEndedStars.gd
+++ subscenes/LevelEndedStars.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var game_scene
+var _todo_after_level
 
 #######################################################
 #
@@ -17,6 +18,7 @@ func _ready():
 #
 #  	This will be public to the GD star_display.gd
 func show_stuff_after_level(info_for_star_calculation):
+	self._decide_what_to_show(info_for_star_calculation)
 	var num_stars = _calculate_stars_for_level(info_for_star_calculation)
 	for i in range(1,num_stars):
 		# instantiate Tile of type TYPE
@@ -24,6 +26,36 @@ func show_stuff_after_level(info_for_star_calculation):
 		# fly it into place
 		pass
 	self._on_last_star_displayed()	# fake emit
+
+#######################################################
+#
+#  	This will determine what to display, and in what order
+func _decide_what_to_show(info_for_star_calc):
+	self._todo_after_level = []
+	if info_for_star_calc['reason'] == G.LEVEL_WIN:
+		self._PlanToDisplayBonus()		# put score on screen so we can edit it
+		self._PlanToReduceSwipes()		# these three can be in any order
+		self._PlanToReduceTiles()		# these three can be in any order (this one blacks out the tiles)
+		self._PlanToAddTimeRemain()		# these three can be in any order
+		self._PlanToDisplayStars()		# add stars below score (or above) or whatever, but should be last
+	else:
+		# this will black out the tiles, but *not* show score because score will not be there
+		self._PlanToReduceTiles()
+
+func _PlanToDisplayBonus():
+	self._todo_after_level.push_back(G.STAR_DISPLAY_BONUS)
+
+func _PlanToReduceSwipes():
+	self._todo_after_level.push_back(G.STAR_REDUCE_SWIPES)
+
+func _PlanToReduceTiles():
+	self._todo_after_level.push_back(G.STAR_REDUCE_TILES)
+
+func _PlanToAddTimeRemain():
+	self._todo_after_level.push_back(G.STAR_ADD_TIME_REMAIN)
+
+func _PlanToDisplayStars():
+	self._todo_after_level.push_back(G.STAR_DISPLAY_STARS)
 
 #######################################################
 #
