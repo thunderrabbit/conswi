01291ffa7ddfd374d776ca2e9fff757f9a077394
Rob Nugen <rob@robnugen.com>
2018-07-29 09:54:50 +0900

    Increase time for level 5 to make it doable

--- levels/normal_welcome_05.gd
+++ levels/normal_welcome_05.gd
@@ -2,6 +2,6 @@ extends "NormalLevel.gd"
 
 func _init():
     max_tiles_avail = 50
-    time_limit_in_sec = 50
+    time_limit_in_sec = 100
     min_swipe_len = 3
     level_requirements = { "square":2 }
