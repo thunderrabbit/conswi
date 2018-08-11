c64d67e9ed77cc6aab2dc09ffcba99db7e4e482f
Rob Nugen <rob@robnugen.com>
2018-08-11 12:10:27 +0900

    Add documentation to finger swipe code

--- subscenes/FingerSwipeShape.gd
+++ subscenes/FingerSwipeShape.gd
@@ -1,7 +1,7 @@
 extends "res://subscenes/SwipeShape.gd"
 
 var show_finger = false		# if true, we will show the finger swiping from first to last tile
-var quantity = 0			# store quantity while finger is swiping
+var quantity = 0			# while finger is swiping, remembers how many of these swipes are needed so they can be displayed by parent class
 
 func swipe_finger():
 	if self.show_finger:
@@ -16,9 +16,9 @@ func swipe_finger():
 		var swipe_array = [Vector2(2,6),Vector2(5,6)]
 		VisibleSwipeOverlay.draw_this_swipe(swipe_array,TileDatabase.tiles[G.TYPE_DOG].ITEM_COLOR)
 	else:
-#		print("no finger")
 		finger_swiped(null, null)			# usually do this; only show finger in first couple of levels
 
+# This display_quantity first displays the finger
 func display_quantity(quantity):
 	self.quantity = quantity
 	print("swipe finger before display quantity")
@@ -26,4 +26,5 @@ func display_quantity(quantity):
 
 func finger_swiped(obj, key):
 	$Finger.set_visible(false)
+	# This is the parent class display_quantity which actually displays the quantity
 	.display_quantity(self.quantity)
