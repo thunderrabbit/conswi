37afedbc4cfddda1ce79d78b2f34acdea380ecf4
Rob Nugen <rob@robnugen.com>
2018-08-11 11:55:30 +0900

    Give finger its own tween Fixes #3

--- subscenes/FingerSwipeShape.gd
+++ subscenes/FingerSwipeShape.gd
@@ -6,12 +6,12 @@ var quantity = 0			# store quantity while finger is swiping
 func swipe_finger():
 	if self.show_finger:
 		$Finger.set_visible(true)
-		$Tween.connect("tween_completed", self, "finger_swiped")
-		$Tween.interpolate_property($Finger, "position",
+		$FingerTween.connect("tween_completed", self, "finger_swiped")
+		$FingerTween.interpolate_property($Finger, "position",
 				Vector2(20,20),		# TODO do not hardcode this
 				Vector2(200,20),    # TODO do not hardcode this
 				1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-		$Tween.start()
+		$FingerTween.start()
 		var swipe_array = [Vector2(2,6),Vector2(5,6)]
 		VisibleSwipeOverlay.draw_this_swipe(swipe_array,TileDatabase.tiles[G.TYPE_DOG].ITEM_COLOR)
 	else:
