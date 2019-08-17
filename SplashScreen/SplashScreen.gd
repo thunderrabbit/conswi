6ba4d384f2b1b7e2e7dd501185331190e207c707
Rob Nugen <rob@robnugen.com>
2019-08-18 02:24:29 +0900

    Loop through all the splash screens
    but crash at the last one

--- SplashScreen/SplashScreen.gd
+++ SplashScreen/SplashScreen.gd
@@ -16,19 +16,37 @@
 extends Node2D
 var gowhere
 
+var splash_texture_1 = preload("res://images/Folder_2.5/01_space@3x.png")
+var splash_texture_2 = preload("res://images/Folder_2.5/02planet@3x_.png")
+var splash_texture_3 = preload("res://images/Folder_2.5/03animal living happily@3x.png")
+var splash_texture_4 = preload("res://images/Folder_2.5/04planet disaster@3x.png")
+var splash_texture_5 = preload("res://images/Folder_2.5/05animal suffering@3x.png")
+var splash_texture_6 = preload("res://images/Folder_2.5/06_wide view@3x.png")
+
 func _ready():
 	$Timer.set_wait_time(G.splashscreen_timeout)
 	$Timer.start()
 
 func _on_SplashScreen_Timer_timeout():
-	if G.current_screen < 2:
-		gowhere = "res://SplashScreen/SplashScreenButton.tscn"
+	if G.current_screen < G.max_spash_screens:
 		G.current_screen = G.current_screen + 1
+		$Timer.start()
 	else:
-		gowhere = "res://LevelSelect/LevelSelectScene.tscn"
+		gowhere = "res://LevelSelect/SplashScreenButton.tscn"
+		SceneSwitcher.goto_scene(gowhere)
 
-	SceneSwitcher.goto_scene(gowhere)
-	
-	
+	if G.current_screen > 0 and G.current_screen <= G.max_spash_screens:
+		$StoryOverlay.visible = true
 
-#	
+	if G.current_screen == 1:
+		$StoryOverlay.texture = splash_texture_1
+	elif G.current_screen == 2:
+		$StoryOverlay.texture = splash_texture_2
+	elif G.current_screen == 3:
+		$StoryOverlay.texture = splash_texture_3
+	elif G.current_screen == 4:
+		$StoryOverlay.texture = splash_texture_4
+	elif G.current_screen == 5:
+		$StoryOverlay.texture = splash_texture_5
+	elif G.current_screen == 6:
+		$StoryOverlay.texture = splash_texture_6
