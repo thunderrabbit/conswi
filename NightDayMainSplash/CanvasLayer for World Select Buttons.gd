66a398c001ddbff3cc8c8248744e15a44fc0b996
Rob Nugen <rob@robnugen.com>
2019-10-20 20:25:29 +0900

    use better name for variable

--- NightDayMainSplash/CanvasLayer for World Select Buttons.gd
+++ NightDayMainSplash/CanvasLayer for World Select Buttons.gd
@@ -21,14 +21,14 @@ func _ready():
 
 func add_world_buttons():
 	var button_width = get_button_width()
-	for i in world_textures:
+	for texture in world_textures:
 		var new_butt = TextureButton.new()
 		new_butt.anchor_left = 0.5
 		new_butt.anchor_right = 0.5
 		new_butt.anchor_top = 0.5
 		new_butt.anchor_bottom = 0.5
-		new_butt.set_normal_texture(i)
-		var texture_size = i.get_size()
+		new_butt.set_normal_texture(texture)
+		var texture_size = texture.get_size()
 		new_butt.set_scale(Vector2(button_width/texture_size.x,button_width/texture_size.y))
 		new_butt.margin_left = -button_width / 2
 		new_butt.margin_right = -button_width / 2
