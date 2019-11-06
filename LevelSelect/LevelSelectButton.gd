339de1b4ccdc02cbae04bea085e981781f8cf78b
Rob Nugen <rob@robnugen.com>
2019-11-06 22:03:01 +0900

    Remove references to icons (stars)
    because the stars are part of the images that show stars

--- LevelSelect/LevelSelectButton.gd
+++ LevelSelect/LevelSelectButton.gd
@@ -47,19 +47,8 @@ func set_button_type(type = G.TYPE_DOG):
 	var path_to_star = String("res://images/levelselect/icons/" + self.my_type_string + "base_2x.png")
 	$starbase.set_texture(load(path_to_star))
 
-func _set_icon_active(icon_num):
-	var which_icon = icon_num	# -1 to account for zero-indexed array `icons`
-	var path_to_icon = String("res://images/levelselect/icons/" + self.my_type_string + String(which_icon) + "star_" + "2x.png")
-	get_node("icon"+String(icon_num)).set_texture(load(path_to_icon))
-
 ##  I actually don't remember if this is being used to scale the images or just position them.
 func scale_to(scale):
-	$icon1.set_position($background.position)	# make sure icons line up with background
-	$icon2.set_position($background.position)	# make sure icons line up with background
-	$icon3.set_position($background.position)	# make sure icons line up with background
 	$level_number.set_scale(scale)
 	$starbase.set_position($level_number.position)
 	$starbase.set_scale(scale)
-	$icon1.set_scale(scale)
-	$icon2.set_scale(scale)
-	$icon3.set_scale(scale)
