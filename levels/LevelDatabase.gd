17f079b9e9ebc95d0731a58b165f633494fe62d7
Rob Nugen <rob@robnugen.com>
2019-12-27 09:23:36 +0900

    Level DB need world_id to uniquely specify level
    Not doing level difficulty yet until needed

--- levels/LevelDatabase.gd
+++ levels/LevelDatabase.gd
@@ -21,7 +21,7 @@ const level_format = "res://levels/%s_%s_%02d.gd"		# normal_welcome_01
 # spin down through smaller numbers to find one.
 # Define levels in `levels/` directory
 # This is never called without params, but I am hinting to future me what must be sent here
-func getExistingLevelGDScript(level_num):
+func getExistingLevelGDScript(which_world_id = TYPE_DOG, level_num = 1):
 	var level_difficulty = "normal"		# TODO add Settings (same as Helpers.gd) and put "normal" and "welcome" into it
 	var level_group = "welcome"			#      Scene > Project Settings > Autoload
 	var level_name = ""
