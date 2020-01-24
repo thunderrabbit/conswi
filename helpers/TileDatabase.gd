02968a54e9e12acd1be3db307d8d70da37917199
Rob Nugen <rob@robnugen.com>
2020-01-24 22:23:59 +0900

    Let TileDatabase convert from tile name to ID
    So in level definitions, we can specify with names which tiles to use.
    
    For #43

--- helpers/TileDatabase.gd
+++ helpers/TileDatabase.gd
@@ -1,5 +1,7 @@
 #    Copyright (C) 2018  Rob Nugen
 #
+#    TileDatabase knows about each type of tile (name and color)
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -14,6 +16,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 extends Node
+
+var idsOfNames = {}		# will be filled in automatically based on TileDatbase.tiles
 
 # Tiles are referenced by constants defined in Globals, and must match the order in items.png
 # e.g.  G.TYPE_PANDA = 0, so Panda must be the zeroth item in array `tiles` below
@@ -68,8 +72,18 @@ var tiles = [
 ]
 
 func _ready():
+    create_name_to_id()
     randomize()
 
 func random_type():
     return randi() % tiles.size()
 
+func create_name_to_id():
+    # fill in idsOfNames so we can look up the name of a swipe given its coordinates
+    for i in range(0, tiles.size()):
+        # get the Item Name as a string to use as a dictionary key in idsOfNames
+        var item_name = tiles[i]["ITEM_NAME"]
+        # if it exists, then presumably there is an error in the ShapeDatabase
+        # (or an error in belief that different swipes produce different arrays)
+        idsOfNames[item_name] = i
+    print(idsOfNames)
