c707ff345e47395cad09f59ee2eb513f46666e49
Rob Nugen <rob@robnugen.com>
2020-01-24 21:46:23 +0900

    Fix Panda world
    Fixes #42

--- helpers/Globals.gd
+++ helpers/Globals.gd
@@ -50,10 +50,10 @@ const TYPE_DOG			= 3
 const TYPE_MONKEY		= 4
 const TYPE_SHEEP		= 5
 const TYPE_PIG			= 6
-const TYPE_CAT			= 0    # CAT is not in items.png or world select screen
-const TYPE_LION			= 0    # Right now, LION and following do not exist in items, but are on world select screen
-const TYPE_RABBIT		= 0
-const TYPE_TIGER		= 0
+const TYPE_CAT			= 90    # CAT is not in items.png or world select screen
+const TYPE_LION			= 90    # Right now, LION and following do not exist in items, but are on world select screen
+const TYPE_RABBIT		= 90
+const TYPE_TIGER		= 90
 
 const STAR_DISPLAY_BONUS = 0
 const STAR_REDUCE_SWIPES = 1
