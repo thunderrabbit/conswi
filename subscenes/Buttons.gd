7b1cb6391e049927960abdab25b1158a8071b16a
Rob Nugen <rob@robnugen.com>
2020-02-26 11:39:12 +0900

    Atom fixed spacing

--- subscenes/Buttons.gd
+++ subscenes/Buttons.gd
@@ -22,34 +22,34 @@ var endLevelButtons = null
 var steering_pad = null
 
 func _init():
-	steering_pad = SteeringPad.instance()
-	add_child(steering_pad)
+    steering_pad = SteeringPad.instance()
+    add_child(steering_pad)
 
-	endLevelButtons = EndLevelBut.instance()
-	add_child(endLevelButtons)
+    endLevelButtons = EndLevelBut.instance()
+    add_child(endLevelButtons)
 
 func grok_input(boolean):
-	steering_pad.set_process_input(boolean)
-	endLevelButtons.set_process_input(boolean)
+    steering_pad.set_process_input(boolean)
+    endLevelButtons.set_process_input(boolean)
 
 func set_game_scene(game_scene):
-	parent_scene = game_scene
-	steering_pad.set_game_scene(parent_scene)
-	endLevelButtons.set_game_scene(parent_scene)
+    parent_scene = game_scene
+    steering_pad.set_game_scene(parent_scene)
+    endLevelButtons.set_game_scene(parent_scene)
 
 func prepare_to_play_level():
-	print("buttons preparing for game")
-	# the steering pad is the left/right buttons at bottom
-	steering_pad.set_position(Helpers.steering_pad_pixels())
-	steering_pad.show()
-	endLevelButtons.hide()
+    print("buttons preparing for game")
+    # the steering pad is the left/right buttons at bottom
+    steering_pad.set_position(Helpers.steering_pad_pixels())
+    steering_pad.show()
+    endLevelButtons.hide()
 
 func level_ended(reason):
-	steering_pad.hide()
-	endLevelButtons.set_process_input(true)
-	# removed set_alignment because it wasn't working when this was a ButtonGroup,
-	# and now that this is a Node2D, it crashes the app
+    steering_pad.hide()
+    endLevelButtons.set_process_input(true)
+    # removed set_alignment because it wasn't working when this was a ButtonGroup,
+    # and now that this is a Node2D, it crashes the app
 #	endLevelButtons.set_alignment(endLevelButtons.ALIGN_CENTER)
-	endLevelButtons.level_over_reason(reason)
-	endLevelButtons.show()
+    endLevelButtons.level_over_reason(reason)
+    endLevelButtons.show()
 
--- subscenes/Player.gd
+++ subscenes/Player.gd
@@ -26,89 +26,89 @@ var nailed = false
 var tile_type = null
 
 func _ready():
-	add_to_group("players")		# to simplify clearing game scene
-	set_process_input(false)
+    add_to_group("players")		# to simplify clearing game scene
+    set_process_input(false)
 
 func set_type(new_tile_type_ordinal):
-	tile_type = new_tile_type_ordinal
-	# instantiate 1 Tile each for our player and shadow.
-	mytile = Segment.instance()
-	mytile.set_tile_type(new_tile_type_ordinal)
-	# add Tile to scene
-	add_child(mytile)
+    tile_type = new_tile_type_ordinal
+    # instantiate 1 Tile each for our player and shadow.
+    mytile = Segment.instance()
+    mytile.set_tile_type(new_tile_type_ordinal)
+    # add Tile to scene
+    add_child(mytile)
 
-	myshadow = Segment.instance()
-	myshadow.set_tile_type(new_tile_type_ordinal)
-	# Tell Tile to tell its sprite it's a shadow
-	myshadow.is_shadow()
-	add_child(myshadow)
+    myshadow = Segment.instance()
+    myshadow.set_tile_type(new_tile_type_ordinal)
+    # Tell Tile to tell its sprite it's a shadow
+    myshadow.is_shadow()
+    add_child(myshadow)
 
 # only the Player tiles will be set draggable (not the shadows)
 # This is set to true by Game.gd
 # This is set to false by Game via nail_player() down below
 func set_draggable(candrag):
-	mytile.set_draggable(candrag)
+    mytile.set_draggable(candrag)
 
 # update player sprite display
 func set_player_position(player_position):
-	my_position = player_position
-	mytile.set_position(Helpers.slot_to_pixels(player_position))
-	if not nailed:
-		# TODO make Helpers.shadowheight or column height
-		myshadow.set_position(Helpers.slot_to_pixels(Vector2(player_position.x, column_height(player_position.x))))   ## shadow
-		if myshadow != null:
-			if should_show_shadow:
-				myshadow.show()
-			else:
-				myshadow.hide()
+    my_position = player_position
+    mytile.set_position(Helpers.slot_to_pixels(player_position))
+    if not nailed:
+        # TODO make Helpers.shadowheight or column height
+        myshadow.set_position(Helpers.slot_to_pixels(Vector2(player_position.x, column_height(player_position.x))))   ## shadow
+        if myshadow != null:
+            if should_show_shadow:
+                myshadow.show()
+            else:
+                myshadow.hide()
 
 # player has been nailed so it should animate or whatever
 func nail_player():
-	# now that we are nailed, we cannot be dragged
-	mytile.set_draggable(false)
-	mytile.set_swipeable(true)
-	
-	# now that we are nailed, we have no shadow
-	myshadow.queue_free()
-	nailed = true
+    # now that we are nailed, we cannot be dragged
+    mytile.set_draggable(false)
+    mytile.set_swipeable(true)
+
+    # now that we are nailed, we have no shadow
+    myshadow.queue_free()
+    nailed = true
 
 func column_height(column):
-	var height = Helpers.slots_down-1
-	for i in range(Helpers.slots_down-1,0,-1):
-		if Helpers.board[Vector2(column, i)] != null:
-			height = i-1
-	return height
+    var height = Helpers.slots_down-1
+    for i in range(Helpers.slots_down-1,0,-1):
+        if Helpers.board[Vector2(column, i)] != null:
+            height = i-1
+    return height
 
 func move_down_if_room():
-	var below_me = my_position + Vector2(0,1)
-	if below_me.y < Helpers.slots_down:
-		if Helpers.board[below_me] == null:
-			Helpers.board[below_me] = self
-			Helpers.board[my_position] = null
-			set_player_position(below_me)
+    var below_me = my_position + Vector2(0,1)
+    if below_me.y < Helpers.slots_down:
+        if Helpers.board[below_me] == null:
+            Helpers.board[below_me] = self
+            Helpers.board[my_position] = null
+            set_player_position(below_me)
 
 func set_show_shadow(should_i):
-	should_show_shadow = should_i
-	# set position forces shdadow to show up or not
-	set_player_position(my_position)
+    should_show_shadow = should_i
+    # set position forces shdadow to show up or not
+    set_player_position(my_position)
 
 func highlight():
-	mytile.highlight()
+    mytile.highlight()
 
 func unhighlight():
-	mytile.unhighlight()
+    mytile.unhighlight()
 
 func darken():
-	mytile.darken()
+    mytile.darken()
 #	mytile.clear_shapes()
 
 func level_ended():
-	if mytouchzone != null:
-		mytouchzone.set_process_input(false)
-		mytouchzone.set_pickable(false)
-	darken()
+    if mytouchzone != null:
+        mytouchzone.set_process_input(false)
+        mytouchzone.set_pickable(false)
+    darken()
 
 func remove_yourself():
-	remove_from_group("players")
-	Helpers.board[my_position] = null
-	mytile.start_swipe_effect()		# release yourself
+    remove_from_group("players")
+    Helpers.board[my_position] = null
+    mytile.start_swipe_effect()		# release yourself
