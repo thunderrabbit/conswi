7be3faa7a62468aa489ebc5bd6fcd211d89fecf1
Rob Nugen <rob@robnugen.com>
2020-03-01 18:26:24 +0900

    Atom fixed whitespace

--- NightDayMainSplash/MainSplashWithGameName.gd
+++ NightDayMainSplash/MainSplashWithGameName.gd
@@ -19,17 +19,17 @@ var day_texture = preload("res://images/Folder_4/universal_background_day@3x.png
 var night_texture = preload("res://images/Folder_4/universal_background_night@3x.png")
 
 func _ready():
-	set_background()
+    set_background()
 
 # set_background keeps _ready() small
 func set_background():
-	var timeDict = OS.get_time();
-	var hour = timeDict.hour;
-	set_background_for_hour(hour)
+    var timeDict = OS.get_time();
+    var hour = timeDict.hour;
+    set_background_for_hour(hour)
 
 # set_background_for_hour keeps things testable
 func set_background_for_hour(var hour):
-	if 6 <= hour && hour < 18:
-		texture = day_texture
-	else:
-		texture = night_texture
+    if 6 <= hour && hour < 18:
+        texture = day_texture
+    else:
+        texture = night_texture
