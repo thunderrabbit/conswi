9a4d4c2e1a3de23c22e188f37fc2e6455d8024e9
Rob Nugen <rob@robnugen.com>
2020-03-08 21:43:06 +0900

    Looks like this is unnecessary

--- SplashScreen/SplashScreen.gd
+++ SplashScreen/SplashScreen.gd
@@ -27,18 +27,37 @@ func _ready():
     $Timer.set_wait_time(G.splashscreen_timeout)
     $Timer.start()
 
-func _on_SplashScreen_Timer_timeout():
-    var viewportHeight = OS.get_window_size().y
-    var viewportWidth = OS.get_window_size().x
+func scalevec(vec):
+    var vecx = vec.x
+    var vecxscale = vec.x / $StoryOverlay.texture.get_size().x
+    var vecy = vec.y #* $StoryOverlay.texture.get_size().x / $StoryOverlay.texture.get_size().y
+    return(Vector2(vecx / vecxscale,vecy / vecxscale))
 
+func boof(text, vec):
+    print(text, vec, scalevec(vec))
+
+func screen_metrics():
+    print("                 [Screen Metrics]")
+ #   boof("            Display size: ", OS.get_screen_size())
+ #   boof("   Decorated Window size: ", OS.get_real_window_size())
+ #   boof("             Window size: ", OS.get_window_size())
+    boof("                viewport: ", get_viewport().size)
+ #   boof("        Project Settings: ", Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height")))
+
+func _on_SplashScreen_Timer_timeout():
+    var viewportHeight = get_viewport().size.y
+    var viewportWidth = get_viewport().size.x
+    screen_metrics()
  #   print(viewportWidth)
-    print(")",viewportWidth, ", ", viewportHeight,"(")
+ #   print(")",viewportWidth, ", ", viewportHeight,"(")
     var scale = 0
     if G.current_screen < G.max_splash_screens:
         G.current_screen = G.current_screen + 1
         $Timer.start()
     else:
         $PawAgreeButton.visible = true
+        G.current_screen = 1
+        $Timer.start()
 
     if G.current_screen > 0 and G.current_screen <= G.max_splash_screens:
         $StoryOverlay.visible = true
@@ -54,24 +73,23 @@ func _on_SplashScreen_Timer_timeout():
     elif G.current_screen == 5:
         $StoryOverlay.texture = splash_texture_5
 
-    print(OS.get_window_size())
-
     print("this is texture size ", $StoryOverlay.texture.get_size())
     scale = viewportWidth / $StoryOverlay.texture.get_size().x
-#    print("scale W", scale)
+    print("scale W", scale)
     scale = viewportHeight / $StoryOverlay.texture.get_size().y
-#    print("scale H", scale)
-    scale = 1
+    print("scale H", scale)
+
+#    $StoryOverlay.set_scale(Vector2(scale, scale))
+
     # Optional: Center the sprite, required only if the sprite's Offset>Centered checkbox is set
-    $StoryOverlay.set_position(Vector2($StoryOverlay.texture.get_size().x/2, $StoryOverlay.texture.get_size().y/2))
+#    $StoryOverlay.set_position(Vector2(viewportWidth/2, viewportHeight/2))
 #    print(Vector2(viewportWidth/2, viewportHeight/2))
 #    print("leafy lefou rainforest")
 
     # $StoryOverlay.set_position(Vector2(0,0))
 
     # Set same scale value horizontally/vertically to maintain aspect ratio
-    $StoryOverlay.set_scale(Vector2(scale, scale))
-
+    print(get_viewport().get_mouse_position())
 
 func _on_PawAgree_button_up():
         gowhere = "res://NightDayMainSplash/NightDayMainSplash.tscn"
