94140a250efb7eb2787fc09a077f57e5c0c31445
Rob Nugen <rob@robnugen.com>
2020-03-08 21:45:08 +0900

    Splash screen sizes working fine it seems

--- SplashScreen/SplashScreen.gd
+++ SplashScreen/SplashScreen.gd
@@ -27,37 +27,14 @@ func _ready():
     $Timer.set_wait_time(G.splashscreen_timeout)
     $Timer.start()
 
-func scalevec(vec):
-    var vecx = vec.x
-    var vecxscale = vec.x / $StoryOverlay.texture.get_size().x
-    var vecy = vec.y #* $StoryOverlay.texture.get_size().x / $StoryOverlay.texture.get_size().y
-    return(Vector2(vecx / vecxscale,vecy / vecxscale))
-
-func boof(text, vec):
-    print(text, vec, scalevec(vec))
-
-func screen_metrics():
-    print("                 [Screen Metrics]")
- #   boof("            Display size: ", OS.get_screen_size())
- #   boof("   Decorated Window size: ", OS.get_real_window_size())
- #   boof("             Window size: ", OS.get_window_size())
-    boof("                viewport: ", get_viewport().size)
- #   boof("        Project Settings: ", Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height")))
-
 func _on_SplashScreen_Timer_timeout():
-    var viewportHeight = get_viewport().size.y
-    var viewportWidth = get_viewport().size.x
-    screen_metrics()
- #   print(viewportWidth)
- #   print(")",viewportWidth, ", ", viewportHeight,"(")
-    var scale = 0
     if G.current_screen < G.max_splash_screens:
         G.current_screen = G.current_screen + 1
         $Timer.start()
     else:
         $PawAgreeButton.visible = true
-        G.current_screen = 1
-        $Timer.start()
+#        G.current_screen = 1
+#        $Timer.start()
 
     if G.current_screen > 0 and G.current_screen <= G.max_splash_screens:
         $StoryOverlay.visible = true
@@ -72,24 +49,6 @@ func _on_SplashScreen_Timer_timeout():
         $StoryOverlay.texture = splash_texture_4
     elif G.current_screen == 5:
         $StoryOverlay.texture = splash_texture_5
-
-    print("this is texture size ", $StoryOverlay.texture.get_size())
-    scale = viewportWidth / $StoryOverlay.texture.get_size().x
-    print("scale W", scale)
-    scale = viewportHeight / $StoryOverlay.texture.get_size().y
-    print("scale H", scale)
-
-#    $StoryOverlay.set_scale(Vector2(scale, scale))
-
-    # Optional: Center the sprite, required only if the sprite's Offset>Centered checkbox is set
-#    $StoryOverlay.set_position(Vector2(viewportWidth/2, viewportHeight/2))
-#    print(Vector2(viewportWidth/2, viewportHeight/2))
-#    print("leafy lefou rainforest")
-
-    # $StoryOverlay.set_position(Vector2(0,0))
-
-    # Set same scale value horizontally/vertically to maintain aspect ratio
-    print(get_viewport().get_mouse_position())
 
 func _on_PawAgree_button_up():
         gowhere = "res://NightDayMainSplash/NightDayMainSplash.tscn"
