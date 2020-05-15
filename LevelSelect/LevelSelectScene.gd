3141d61c7e60ff18cfbf573b5879fa1e577edbfc
Rob Nugen <rob@robnugen.com>
2020-05-15 18:34:40 +0900

    Allow buttons to spread across screen
    Fixes #50

--- LevelSelect/LevelSelectScene.gd
+++ LevelSelect/LevelSelectScene.gd
@@ -21,12 +21,6 @@ extends Node2D
 # here so it is clear that they are only used here.
 # Feel free to arrange these buttons in a better way.
 
-const SLOT_SIZE       = 140
-var left_space        = 70
-var top_space         = 190
-var slot_gap          = 60
-var slot_gap_h        = slot_gap
-var slot_gap_v        = slot_gap * 2
 var buttons_across    = 3
 var num_buttons       = 12
 var world_name        = 0
@@ -37,6 +31,17 @@ func _init():
     var world_type = get_world_type()
     set_world_background(world_type)
     add_buttons_to_scene(world_type)
+
+func _slot_size():
+    var os_window_width = OS.get_window_size().x
+    var one_third_of_screen = os_window_width / buttons_across
+    return one_third_of_screen
+
+func _left_space():
+    return _slot_size() / 2
+
+func _top_space():
+    return _slot_size()
 
 func get_world_type():
     # Helpers.requested_world was set by WorldSelectScene
@@ -55,10 +60,10 @@ func add_buttons_to_scene(button_type):
         var level_but = LevelSelectButton.instance()
         level_but.set_position(button_loc)
         level_but.set_level(level)
-        level_but.set_size(Vector2(SLOT_SIZE,SLOT_SIZE))
+        level_but.set_size(Vector2(_slot_size(),_slot_size()))
         level_but.set_num_stars(Savior.read_num_stars(self.get_world_type(),level))
         level_but.set_button_type(button_type)
-        level_but.scale_to(Vector2(0.5,0.5))
+        level_but.scale_to(Vector2(1,1))
         level_but.connect("pressed",self,"_on_Button_pressed",[level])
         add_child(level_but)
 
@@ -66,8 +71,8 @@ func level_to_pixels(level):
     # level-1 because we start with 1 above.  Otherwise gap appears in top left corner
     level = level - 1
     var slot = Vector2(level % buttons_across, level / buttons_across)
-    return Vector2(left_space+(SLOT_SIZE + slot_gap_h)*(slot.x),
-                    top_space+(SLOT_SIZE + slot_gap_v)*(slot.y))
+    return Vector2(_left_space()+_slot_size()*(slot.x),
+                    _top_space()+_slot_size()*(slot.y))
 
 func _on_Button_pressed(level):
     Helpers.requested_level = level
