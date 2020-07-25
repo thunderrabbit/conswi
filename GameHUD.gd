4758e879f18a9718e6275ecc90015f9e32a4d66e
Rob Nugen <rob@robnugen.com>
2020-07-25 19:01:34 +0900

    Update copyright year

--- GameHUD.gd
+++ GameHUD.gd
@@ -1,4 +1,4 @@
-#    Copyright (C) 2018  Rob Nugen
+#    Copyright (C) 2020 Rob Nugen
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
--- GameSwipeDetector.gd
+++ GameSwipeDetector.gd
@@ -1,4 +1,4 @@
-#    Copyright (C) 2019  Rob Nugen
+#    Copyright (C) 2020  Rob Nugen
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
