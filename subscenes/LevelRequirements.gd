7262c475b4e2bf4b7de84d8204c0f0581ad34317
Rob Nugen <rob@robnugen.com>
2020-11-14 13:44:04 +0900

    Create empty LevelRequirements files
    and expand the margins for the .tscn to match project settings

---
+++ subscenes/LevelRequirements.gd
@@ -0,0 +1,30 @@
+#    Copyright (C) 2020  Rob Nugen
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+extends MarginContainer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    pass
+
+#func _process(delta):
+#    # Called every frame. Delta is time since last frame.
+#    # Update game logic here.
+#    pass
