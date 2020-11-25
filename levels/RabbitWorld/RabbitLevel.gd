9a16c8311df805a5a5c6be505f74ae90da0e9c38
Rob Nugen <rob@robnugen.com>
2020-11-26 00:16:32 +0900

    Update copyright to 2020

--- levels/RabbitWorld/RabbitLevel.gd
+++ levels/RabbitWorld/RabbitLevel.gd
@@ -1,6 +1,6 @@
 #    Sends rabbit tiles to any level that extends (inherits) this class
 #
-#    Copyright (C) 2019  Rob Nugen
+#    Copyright (C) 2020 Rob Nugen
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
