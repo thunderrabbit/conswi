d9cf671d066092ec633b19d3ac7a7bed49f4d6bd
Rob Nugen <rob@robnugen.com>
2020-12-05 18:12:30 +0900

    Name things consistently

--- helpers/named_swipes/named_swipes.gd
+++ helpers/named_swipes/named_swipes.gd
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-var named_shapes = {
+var named_swipes = {
     "pink_floyd": [7,
     1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,
