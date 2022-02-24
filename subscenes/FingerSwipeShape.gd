5433d373fb149a9037b27c44f6a4d40132978e93
Rob Nugen <rob@robnugen.com>
2022-02-24 16:16:21 +0900

    Create .import files for level numbers, including wrong named files
    https://github.com/OneandonlyDpen/conswi/issues/3

--- subscenes/FingerSwipeShape.gd
+++ subscenes/FingerSwipeShape.gd
@@ -32,31 +32,31 @@ var quantity = 0			# while finger is swiping, remembers how many of these swipes
 # func finger_moved_a_bit updates the swipe to match the finger.
 # It is in here that we should see about highlighting pieces that have been reached by the finger
 func finger_moved_a_bit ( object, key, elapsed, value ):
-	var swipe_array = [Helpers.offset_bottom_center_slot(dimensions),Helpers.pixels_to_slot($Finger.get_global_position())]
-	VisibleSwipeOverlay.draw_this_swipe(swipe_array,TileDatabase.tiles[G.TYPE_DOG].ITEM_COLOR,false)	# false = do not add mouse pos to swipe
+    var swipe_array = [Helpers.offset_bottom_center_slot(dimensions),Helpers.pixels_to_slot($Finger.get_global_position())]
+    VisibleSwipeOverlay.draw_this_swipe(swipe_array,TileDatabase.tiles[G.TYPE_DOG].ITEM_COLOR,false)	# false = do not add mouse pos to swipe
 
 func swipe_finger():
-	if self.show_finger:
-		$Finger.set_visible(true)
-		$FingerTween.connect("tween_completed", self, "finger_swiped")
-		$FingerTween.connect("tween_step", self, "finger_moved_a_bit")
-		# Move finger relative to the swipe.  (NOTE This only works for horizontal swipes)
-		$FingerTween.interpolate_property($Finger, "position",
-				Vector2(0,0),									# finger start position, relative to the swiped piece
-				Vector2(G.GameGridSlotSize() * (dimensions.x+1),0),		# finger end position.  dimensions.x is the width of piece (starting from 0)
-				G.finger_swipe_duration,						# Duration of tween
-				Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-		$FingerTween.start()
-	else:
-		finger_swiped(null, null)			# usually do this; only show finger in first couple of levels
+    if self.show_finger:
+        $Finger.set_visible(true)
+        $FingerTween.connect("tween_completed", self, "finger_swiped")
+        $FingerTween.connect("tween_step", self, "finger_moved_a_bit")
+        # Move finger relative to the swipe.  (NOTE This only works for horizontal swipes)
+        $FingerTween.interpolate_property($Finger, "position",
+                Vector2(0,0),									# finger start position, relative to the swiped piece
+                Vector2(G.GameGridSlotSize() * (dimensions.x+1),0),		# finger end position.  dimensions.x is the width of piece (starting from 0)
+                G.finger_swipe_duration,						# Duration of tween
+                Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+        $FingerTween.start()
+    else:
+        finger_swiped(null, null)			# usually do this; only show finger in first couple of levels
 
 # This display_quantity first displays the finger
 func display_quantity(quantity):
-	self.quantity = quantity
-	print("swipe finger before display quantity")
-	swipe_finger()
+    self.quantity = quantity
+    print("swipe finger before display quantity")
+    swipe_finger()
 
 func finger_swiped(obj, key):
-	$Finger.set_visible(false)
-	# This is the parent class display_quantity which actually displays the quantity
-	.display_quantity(self.quantity)
+    $Finger.set_visible(false)
+    # This is the parent class display_quantity which actually displays the quantity
+    .display_quantity(self.quantity)
